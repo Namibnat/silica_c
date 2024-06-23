52b375d1e8091d368d3e8e036ed4412299109537
Vernon Swanepoel <frantic.naturalist@gmail.com>
2024-06-23 21:29:18 +0100

    Starting work towards token structs

---
+++ doodle/work_to_token_finder/tokens.c
@@ -0,0 +1,154 @@
+/* Identify  */
+
+#include <stdio.h>
+#include <stdbool.h>
+
+typedef struct {
+    char token_type;
+    char *token_text;
+} Token;
+
+typedef Token *Tokens;
+
+typedef enum {
+    START_SOURCE,
+    IN_BRACKETS,
+    IN_CURLY_BRACKETS,
+    IN_NEW_STATEMENT,
+    IN_DIGIT,
+} item_state;
+
+
+bool is_valid_text_start(char c) {
+    return ((c >= 0x41 && c <= 0x5A) || (c >= 0x61 && c <= 0x7A));
+}
+
+
+bool is_valid_text_inner(char c) {
+    return ((c >= 0x41 && c <= 0x5A) ||
+            (c >= 0x61 && c <= 0x7A) ||
+            (c == 0x2D) ||
+            (c >= 0x30 && c <= 0x39));
+}
+
+
+bool is_open_bracket(char c) {
+    return (c == 0x28);
+}
+
+
+bool is_close_bracket(char c) {
+    return (c == 0x29);
+}
+
+
+bool is_open_curly_brackets(char c) {
+    return (c == 0x7B);
+}
+
+
+bool is_close_curly_bracket(char c) {
+    return (c == 0x7D);
+}
+
+
+bool is_digit(char c) {
+    return (c >= 0x30 && c <= 0x39);
+}
+
+
+bool is_end_statement(char c) {
+    return (c == 0x3B);
+}
+
+
+int main(int argc, char **argv)
+{
+    char source[] = "int main(){return 0;}";
+
+    int state_stack[] = {START_SOURCE, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1};
+    int stack_end = 18;
+    int stack_start = 0;
+    int stack_pointer = stack_start;
+    int i = 0;
+    int j;
+    char c;
+
+    do  {
+        c = source[i];
+
+        if (is_valid_text_start(c)) {
+            while (is_valid_text_inner(c)) {
+                putchar(c);
+                c = source[++i];
+            }
+            printf("\n");
+            c = source[--i];
+
+        } else if (is_open_bracket(c)) {
+            putchar(c);
+            state_stack[++stack_pointer] = IN_BRACKETS;
+            printf(" : STACK STATE NOW ");
+
+            for (j=stack_start; j<=stack_pointer; j++) {
+                printf("(%d) ", state_stack[j]);
+            }
+            printf("\n\n");
+
+        } else if (is_close_bracket(c)) {
+            putchar(c);
+            stack_pointer--;
+            printf(" : STACK STATE NOW ");
+
+            for (j=stack_start; j<=stack_pointer; j++) {
+                printf("(%d) ", state_stack[j]);
+            }
+            printf("\n");
+
+        } else if (is_open_curly_brackets(c)) {
+            putchar(c);
+            state_stack[++stack_pointer] = IN_CURLY_BRACKETS;
+            state_stack[++stack_pointer] = IN_NEW_STATEMENT;
+            printf(" : STACK STATE NOW ");
+
+            for (j=stack_start; j<=stack_pointer; j++) {
+                printf("(%d) ", state_stack[j]);
+            }
+            printf("\n");
+
+        } else if (is_close_curly_bracket(c)) {
+            putchar(c);
+            stack_pointer--;
+            printf(" : STACK STATE NOW ");
+
+            for (j=stack_start; j<=stack_pointer; j++) {
+                printf("(%d) ", state_stack[j]);
+            }
+            printf("\n");
+
+        } else if (is_digit(c)) {
+            while (is_digit(c)) {
+                putchar(c);
+                c = source[++i];
+            }
+            c = source[--i];
+            printf("\n");
+
+        } else if (is_end_statement(c)) {
+            putchar(c);
+            stack_pointer--;
+            printf(" : STACK STATE NOW ");
+
+            for (j=stack_start; j<=stack_pointer; j++) {
+                printf("(%d) ", state_stack[j]);
+            }
+
+            printf("\n");
+        }
+
+        i++;
+    } while (c != '\0');
+
+    return 0;
+}
+
